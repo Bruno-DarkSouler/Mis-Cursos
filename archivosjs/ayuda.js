acc62b42ea0324654c1e8986bcfa94ed3c237c28
Mateo781 <mateo.silvero.t1vl@gmail.com>
2024-09-13 16:26:32 -0300

    commit (agregado de js y links de ventanas)

---
+++ archivosjs/ayuda.js
@@ -0,0 +1,15 @@
+
+const titulo = document.querySelectorAll('.titulo');
+
+
+titulo.forEach(titulo => {
+    titulo.addEventListener('click', function() {
+     
+        const respuesta = this.nextElementSibling;
+        respuesta.style.display = respuesta.style.display === 'block' ? 'none' : 'block';
+        const flecha = this.querySelector('.flecha');
+        flecha.style.transform = flecha.style.transform === 'rotate(180deg)' ? 'rotate(0deg)' : 'rotate(180deg)';
+
+    });
+});
+
