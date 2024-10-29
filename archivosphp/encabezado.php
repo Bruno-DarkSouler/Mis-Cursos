3da4c39aaf534ae7d035f70c30e98a6393e1730b
Mateo781 <mateo.silvero.t1vl@gmail.com>
2024-10-29 15:46:14 -0300

    commit (agregado de encabezado.php para usar)

---
+++ archivosphp/encabezado.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>encabezado</title>
+    <link rel="stylesheet" href="../estilos/encabezado.css">
+</head>
+<body>
+    
+<nav>
+                <div class="logo-titulo">
+                    <img class="logo" src="../imagenes/logo_sin_fondo2.png" alt="">
+                    <div class="titulo"><h3>Mis Cursos</h3></div>
+                </div>
+                <div class="opciones">
+                    <a href=""><h3 class="precios">Precios</h3></a>
+                    <a href="./paginas/ayuda.html"><h3 class="ayuda">Ayuda</h3></a>
+                    <a href="fundadores.html"><h3 class="fundadores">Fundadores</h3></a>
+                </div>
+                <div class="logeo">
+                    <a href="../paginas/iniciosesion.html"><div class="iniciar-sesion"><h3>iniciar sesión</h3></div></a>
+                    <a href="../paginas/registrousuario.html"><div class="crear-cuenta"><h3 class="letra-crear-cuenta">Crear cuenta</h3></div></a>
+                </div>
+            </nav>
+
+</body>
+</html>
