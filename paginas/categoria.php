1ec296d56779e538e4cb4c8dc4bc1076abcf6277
Mateo781 <mateo.silvero.t1vl@gmail.com>
2024-11-08 17:55:39 -0300

    commit (categoria desarrollado, perfil desarrollado)

--- paginas/categoria.php
+++ paginas/categoria.php
@@ -4,44 +4,102 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cursos de Programación</title>
-    <link rel="stylesheet" href="./estilos/categoria.css">
+    <link rel="stylesheet" href="../estilos/categoria.css">
 </head>
 <body>
-
     <header>
-        <!-- Encabezado del sitio -->
-        <h1>Cursos de Programación</h1>
+        <?php
+        require "../archivosphp/encabezado.php";
+        ?>
     </header>
 
-    <!-- Barra de búsqueda -->
-    <div class="contenedor-buscador">
-        <input 
-            type="text" 
-            id="buscador" 
-            class="campo-busqueda" 
-            placeholder="Buscar cursos..."
-        >
-    </div>
+    <main>
 
-    <!-- Sección principal -->
-    <section class="seccion-principal">
-        <h2>Explora los mejores precios y lenguajes de todo el mundo</h2>
-        <p>Siempre al mejor precio</p>
-    </section>
+    <div class="principal">
 
-    <!-- Contenedor de Cursos -->
-    <div class="contenedor-cursos" id="contenedor-cursos"></div>
-
-    <!-- Modal de información del curso -->
-    <div class="modal" id="modal">
-        <div class="modal-content">
-            <span class="close" id="close-modal">&times;</span>
-            <h2 id="modal-title"></h2>
-            <p id="modal-description"></p>
+        <div class="lenguaje">
+            <div class="imagen"><img src="../imagenes/logo_c++.png" alt=""></div>
+            <div class="descripcion">
+                <h1 class="titulo_lenguaje">C++</h1>
+                <hr>
+                <p class="texto_lenguaje">C++ es un lenguaje de programación diseñado en 1979 por Bjarne Stroustrup. La intención de su creación fue extender al lenguaje de programación C y añadir mecanismos que permiten la manipulación de objetos.</p>
+            </div>
         </div>
-    </div>
 
-    <!-- Script de JavaScript -->
-    <script src="./archivosjs/categoria.js"></script>
+        <div class="lenguaje">
+            <div class="imagen"><img src="../imagenes/logo_c.png" alt=""></div>
+            <div class="descripcion">
+                <h1 class="titulo_lenguaje">C</h1>
+                <hr>
+                <p class="texto_lenguaje">C es un lenguaje de programación de propósito general​ originalmente desarrollado por Dennis Ritchie entre 1969 y 1972 en los Laboratorios Bell.</p>
+            </div>
+        </div>
+
+        <div class="lenguaje">
+            <div class="imagen"><img src="../imagenes/logo_python.png" alt=""></div>
+            <div class="descripcion">
+                <h1 class="titulo_lenguaje">Python</h1>
+                <hr>
+                <p class="texto_lenguaje">Python es un lenguaje de alto nivel de programación interpretado cuya filosofía hace hincapié en la legibilidad de su código.</p>
+            </div>
+        </div>
+
+        <div class="lenguaje">
+            <div class="imagen"><img src="../imagenes/logo_html.png" alt=""></div>
+            <div class="descripcion">
+            <h1 class="titulo_lenguaje">HTML</h1>
+            <hr>
+            <p class="texto_lenguaje">HTML, acrónimo en inglés de HyperText Markup Language, hace referencia al lenguaje de marcado utilizado en la creación de páginas web.</p>
+            </div>
+        </div>
+
+        <div class="lenguaje">
+            <div class="imagen"><img src="../imagenes/logo_css.png" alt=""></div>
+            <div class="descripcion">
+                <h1 class="titulo_lenguaje">CSS</h1>
+                <hr>
+                <p class="texto_lenguaje">CSS es un lenguaje informático especializado en definir y cohesionar la presentación de un documento escrito en un lenguaje de marcado como HTML o XML.</p>
+            </div>
+        </div>
+
+        <div class="lenguaje">
+            <div class="imagen"><img src="../imagenes/logo_js.webp" alt=""></div>
+            <div class="descripcion">
+                <h1 class="titulo_lenguaje">JavaScript</h1>
+                <hr>
+                <p class="texto_lenguaje">JavaScript es un lenguaje de programación interpretado, dialecto del estándar ECMAScript. Se define como orientado a objetos, ​ basado en prototipos, imperativo, débilmente tipado y dinámico.</p>
+            </div>
+        </div>
+
+        <div class="lenguaje">
+            <div class="imagen"><img src="../imagenes/logo_mongodb.png" alt=""></div>
+            <div class="descripcion">
+                <h1 class="titulo_lenguaje">MongoDB</h1>
+                <hr>
+                <p class="texto_lenguaje">MongoDB es un sistema de bases de datos noSQL, no relacional.</p>
+            </div>
+        </div>
+
+        <div class="lenguaje">
+            <div class="imagen"><img src="../imagenes/logo_mysql.png" alt=""></div>
+            <div class="descripcion">
+                <h1 class="titulo_lenguaje">MySQL</h1>
+                <hr>
+                <p class="texto_lenguaje">MySQL es un sistema de gestión de bases de datos relacional desarrollado bajo licencia dual: Licencia pública general/Licencia comercial por Oracle Corporation.</p>
+            </div>
+        </div>
+
+        <div class="lenguaje">
+            <div class="imagen"><img src="../imagenes/logo_java.png" alt=""></div>
+            <div class="descripcion">
+                <h1 class="titulo_lenguaje">Java</h1>
+                <hr>
+                <p class="texto_lenguaje">Java es un lenguaje de programación y una plataforma informática que fue comercializada por primera vez en 1995 por Sun Microsystems.​​</p>
+            </div>
+        </div>
+
+    </div>
+        
+    </main>
 </body>
 </html>
