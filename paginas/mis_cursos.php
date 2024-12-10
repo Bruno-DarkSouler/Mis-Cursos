e77ff5bd45c54caace44c760a551db143dab14c0
Mateo781 <mateo.silvero.t1vl@gmail.com>
2024-12-09 22:52:58 -0300

    commit (Diseño los cursos y las tareas)

---
+++ paginas/mis_cursos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilos/mis_cursos.css">
+    <title>Mis Cursos</title>
+</head>
+<body>
+    <div class="curso">
+        <div class="img">
+            <img class="imagen_perfil" src="../imagenes/logo_perfil_sin_fondo.png" alt="">
+        </div>
+        <div class="nombre"><p>nombre del curso</p></div>
+        <div class="categoria">Categoria: categoria</div>
+        <div class="instructor">Instructor: nombre y apellido del instructor</div>
+        <div class="descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id minus tempora dolorum in neque dolorem nihil animi hic. Ex totam, aliquid doloribus dolor cum asperiores velit? Repellat blanditiis laborum repellendus?</div>
+    </div>
+</body>
+</html>
